79c93a1e721efb1d6f96e66a205c50a74231dc83
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-07-18 14:06:17 +0300

    :sparkles: Feat: added working sample page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@
 import "@/styles/app.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
+import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -10,14 +12,17 @@ Amplify.configure(outputs, {
 	ssr: true
 });
 
-// export default function App({ Component, pageProps }: AppProps) {
-// 	return <Component {...pageProps} />;
-// }
 
-export default function RootLayoutThatConfiguresAmplifyOnTheClient({
-	children
-}: {
-	children: React.ReactNode;
-}) {
-	return children;
+
+function App({ Component, pageProps, signOut, user }: WithAuthenticatorProps) {
+	return (
+		<>
+			<h1>Hello {user?.username}</h1>
+			<button onClick={signOut}>Sign out</button>
+			<Component {...pageProps} />
+		</>
+	);
 }
+
+export default withAuthenticator(App);
+
