6c117d51025d2680286adce90aac40f31dc98289
Lint Action <lint-action@samuelmeuli.com>
2024-09-18 19:51:26 +0000

    Fix code style issues with Black

--- src/apps/landing_page/models.py
+++ src/apps/landing_page/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     link = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
 
+
 class User(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    avatar = models.ImageField(name=f"{name}-avatar",width_field=300,height_field=300)
+    avatar = models.ImageField(name=f"{name}-avatar", width_field=300, height_field=300)
     skills = models.CharField(max_length=200)
-    projects = models.ForeignKey(Project, on_delete=models.RESTRICT) # TODO: Check what restrict does
+    projects = models.ForeignKey(
+        Project, on_delete=models.RESTRICT
+    )  # TODO: Check what restrict does
