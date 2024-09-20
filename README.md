bb880a4819c2e56a0c06ff5ab2758d00d7f00d20
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2024-09-20 19:50:06 +0300

    Feat/django app controller (#7)
    * :bug: fix: dummy_src
    
    * Fix code style issues with Black
    
    * :sparkles: feat: added urlpattern & utils
    
    * :wrench: chore: added .envrc to gitignore
    
    * :rocket: feat: added Jenkinsfile
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * Fix code style issues with Black
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :bug: fix: Jenkins
    
    * :bug: fix: Jenkins
    
    * :green_heart: fix: Jenkins
    
    * :sparkles: fix: Jenkins
    
    * :sparkles: fix: Jenkins
    
    * :bug: fix: Jenkins
    
    * :bug: fix: Sonar
    
    * :zap: feat: Jenkins agent via cloud
    
    * :sparkles: feat: Jenkins agent via cloud
    
    * :green_heart: fix: Sonar
    
    * :green_heart: fix: Jenkins JAVA
    
    * :green_heart: fix: Jenkins JAVA
    
    * :bug: fix: Jenkins JAVA
    
    * :bug: fix: Jenkins JAVA
    
    * :green_heart: fix: removed JAVA version
    
    * :bug: fix: removed JAVA version
    
    * :bug: fix: removed JAVA version
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :sparkles: fix: removed agent
    
    * :pencil2: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :bug: fix: removed agent
    
    * :sparkles: fix: removed agent
    
    * :sparkles: fix: removed agent
    
    * :pencil2: fix: removed agent
    
    * :sparkles: feat: added SonarCloud scanner to github actions
    
    * :green_heart: fix: sonarCloud workflow
    
    * :green_heart: fix: src
    
    * :bug: fix: SonarQube
    
    * :green_heart: fix: SonarQube
    
    * :bug: fix: SonarQube
    
    * :green_heart: fix: SonarQube
    
    * :bug: fix: SonarQube
    
    * :green_heart: fix: SonarQube
    
    * :bug: fix: SonarQube
    
    * :bug: fix: SonarQube
    
    * :bug: fix: SonarQube token
    
    * :wastebasket: chore: removed commented code for Jenkinsfile
    
    * :pencil2: fix: missing bracket
    
    * :green_heart: fix: Jenkinsfile
    
    * :bug: fix: Jenkinsfile
    
    * :bug: fix: Jenkinsfile
    
    * :fire: chore: clean Jenkins comments
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=IliyanKostov9_portfolio&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=IliyanKostov9_portfolio)
+
 # portfolio
--- shell.nix
+++ shell.nix
@@ -1,17 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
+
 pkgs.mkShell {
-  buildInputs = [
-    (pkgs.buildFHSUserEnv {
-      name = "pipzone";
-      targetPkgs = pkgs: (with pkgs; [
-        python311
-        pdm
-        zsh
-      ]);
-      runScript = "zsh";
-      initHook = ''
-        echo "Setting up environment..."
-      '';
-    })
+  packages = with pkgs; [
+    python311
+    pdm
+    zsh
   ];
+
+  runScript = "zsh";
+  shellHook = ''
+    export PYTHONPATH="$(pwd):$(pwd)/src/apps"
+  '';
 }
