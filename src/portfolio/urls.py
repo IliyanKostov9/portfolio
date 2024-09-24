6806d497c61a21d41d78444bb2a88fe50c40546c
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2024-09-24 19:48:18 +0300

    Feat/lint jenkinsfile (#15)
    * 🚀 Feat: updated Jenkinsfile
    
    * ✨ Feat: added Python tool
    
    * ✏️ Fix: typo tool
    
    * 🐛 Fix: env
    
    * 🐛 Fix: env
    
    * 🐛 Fix: env
    
    * 🔥 Fix: env
    
    * 🐛 Fix: env
    
    * 🐛 Fix: env
    
    * 🎨 Fix: env
    
    * 🎨 Fix: env
    
    * 🚨 Chore: removed env
    
    * ✨ Feat: reverted back to working python3 path
    
    * 👔 Feat: added timeout for jenkins agents
    
    * 🐛 Fix: timeout issue
    
    * 🐛 Fix: lint issue
    
    * ✨ Feat: pyre
    
    * 🩹 Fix: Jenkins syntax issue
    
    * 🚨 Fix: permission error
    
    * 🚀 Feat: added circleci
    
    * 💚 Fix: activate venv
    
    * 💚 Fix: venv
    
    * 💚 Fix: venv
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :bug: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :bug: fix: circleCI
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: circleci venv cache
    
    * :green_heart: fix: pyre
    
    * :pushpin: feat: added requirements.txt
    
    * :heavy_plus_sign: fix: added wheel
    
    * :bug: fix: added wheel
    
    * :pushpin: fix: image
    
    * :zap: feat: added pyre cache on circleci
    
    * :green_heart: fix: broken pipe on pyre
    
    * :bug: fix: sonarqube coverage analysis
    
    * :green_heart: fix: circleci cache

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -18,5 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-urlpatterns = [path("admin/", admin.site.urls), path("", include("landing_page.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),  # pyre-ignore[16]
+    path("", include("landing_page.urls")),
+]
