009b3ab9556e38fcfa97db7d1e97da8c156f30aa
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-25 23:35:12 +0300

    fix: black formatter

--- src/apps/landing_page/models.py
+++ src/apps/landing_page/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Project(models.Model):
-    name = models.CharField("project name",max_length=200)
-    link = models.CharField("Link to project",max_length=100)
-    description = models.CharField("Short description of the project",max_length=2000)
+    name = models.CharField("project name", max_length=200)
+    link = models.CharField("Link to project", max_length=100)
+    description = models.CharField("Short description of the project", max_length=2000)
 
 
 class User(models.Model):
-    name = models.CharField("Name of the user",max_length=200)
-    email = models.EmailField("Email of the user",max_length=200)
+    name = models.CharField("Name of the user", max_length=200)
+    email = models.EmailField("Email of the user", max_length=200)
     avatar = models.ImageField(name=f"{name}-avatar", width_field=300, height_field=300)
-    skills = models.CharField("Skillsets",max_length=200)
+    skills = models.CharField("Skillsets", max_length=200)
     projects = models.ForeignKey(
         Project, on_delete=models.RESTRICT
     )  # TODO: Check what restrict does
