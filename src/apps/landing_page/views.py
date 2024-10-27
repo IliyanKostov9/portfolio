8901a661312aea8cfaa286156d960aaa58f9577f
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-27 19:05:16 +0200

    fix: sonarqube tests

--- src/apps/landing_page/views.py
+++ src/apps/landing_page/views.py
@@ -1,19 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def home(request):
     template = loader.get_template("landing_page/home.html")
     return HttpResponse(template.render({}, request))
 
 
+@require_http_methods(["GET"])
 def projects():
     return HttpResponse("Projects")
 
 
+@require_http_methods(["GET", "POST"])
 def contact():
     return HttpResponse("Contact")
 
 
+@require_http_methods(["GET"])
 def about():
     return HttpResponse("About")
