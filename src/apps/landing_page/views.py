b66faf53a23cc60da2898232fc2c8391f70c8d81
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-03-17 09:52:21 +0000

    Feat/layout (#76)
    * feat: added bootstrap template
    
    * feat: added basic layout
    
    * feat: added
    
    * feat: added dependabot deps
    
    * chore: removed profile image
    
    * feat: added forgejo

--- src/apps/landing_page/views.py
+++ src/apps/landing_page/views.py
@@ -1,11 +1,14 @@
+from typing import Final
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.http import require_http_methods, require_POST
 
+APP_NAME: Final[str] = "landing_page"
+
 
 @require_http_methods(["GET"])
 def home(request):
-    template = loader.get_template("landing_page/home.html")
+    template = loader.get_template(f"{APP_NAME}/home.html")
     return HttpResponse(template.render({}, request))
 
 
