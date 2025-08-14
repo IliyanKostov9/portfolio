307f9927d5c343fa7e7ec10c7919026d435ef42e
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-08-14 23:51:25 +0200

    Feature/models technology about (#121)
    * refactor: home html into seperate files
    
    D  src/apps/landing_page/templates/pages/home.html
    A  src/apps/landing_page/templates/pages/home/about.html
    A  src/apps/landing_page/templates/pages/home/certification.html
    A  src/apps/landing_page/templates/pages/home/contact_me.html
    A  src/apps/landing_page/templates/pages/home/education.html
    A  src/apps/landing_page/templates/pages/home/index.html
    A  src/apps/landing_page/templates/pages/home/projects.html
    A  src/apps/landing_page/templates/pages/home/work_history.html
    M  src/apps/landing_page/views/home/home_view.py
    A  src/static/scripts/domContextLoaded.js
    
    * feat: added models & migrations
    
    M  Makefile
    A  src/apps/landing_page/config/portfolio/technology.yaml
    A  src/apps/landing_page/config/portfolio/technology_category.yaml
    D  src/apps/landing_page/config/projects.yaml
    A  src/apps/landing_page/data_class/portfolio/portfolio.py
    A  src/apps/landing_page/data_class/portfolio/technology.py
    A  src/apps/landing_page/data_class/portfolio/technology_category.py
    A  src/apps/landing_page/migrations/0002_remove_user_projects_delete_project_delete_user.py
    A  src/apps/landing_page/migrations/0003_initial.py
    A  src/apps/landing_page/migrations/0004_auto_20250814_1359.py
    A  src/apps/landing_page/migrations/0005_rename_technologystack_technology.py
    A  src/apps/landing_page/models/__init__.py
    D  src/apps/landing_page/models/models.py
    A  src/apps/landing_page/models/technology.py
    A  src/apps/landing_page/models/technology_category.py
    M  src/apps/landing_page/views/home/home_view.py
    
    * feat: added templating
    
    M  src/apps/landing_page/config/portfolio/technology.yaml
    M  src/apps/landing_page/data_class/portfolio/technology.py
    M  src/apps/landing_page/migrations/0004_auto_20250814_1359.py
    A  src/apps/landing_page/migrations/0004_technology_delete_technologystack.py
    A  src/apps/landing_page/migrations/0005_auto_20250814_1804.py
    D  src/apps/landing_page/migrations/0005_rename_technologystack_technology.py
    A  src/apps/landing_page/migrations/0006_merge_0004_auto_20250814_1359_0005_auto_20250814_1804.py
    M  src/apps/landing_page/migrations/__init__.py
    M  src/apps/landing_page/models/technology.py
    M  src/apps/landing_page/templates/pages/home/about.html
    M  src/apps/landing_page/views/home/home_view.py
    
    * fix: tech stack
    
    M  src/apps/landing_page/config/portfolio/technology.yaml
    M  src/apps/landing_page/templates/pages/home/about.html
    M  src/apps/landing_page/views/home/home_view.py

--- src/apps/landing_page/views/home/home_view.py
+++ src/apps/landing_page/views/home/home_view.py
@@ -1,12 +1,55 @@
+from collections import defaultdict
 from typing import Any
 
 from django.http import HttpResponse
 from django.template import loader
 from django.views import View
+from landing_page.models.technology import Technology
 
 
 class HomeView(View):
     def get(self, request: Any) -> HttpResponse:
-        template = loader.get_template("pages/home.html")
+        template = loader.get_template("pages/home/index.html")
+        technologies = self._get_technologies()
 
-        return HttpResponse(template.render({}, request))
+        context: dict[str, Any] = {"technologies": technologies}
+
+        return HttpResponse(template.render(context, request))
+
+    def _get_technologies(self) -> Any:
+        technologies_objs = list(Technology.objects.all().values())
+        pages = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+        for tech in technologies_objs:
+            page = tech["page"]
+            category = tech["category_id"]
+            row = tech["row"]
+
+            item_data = {
+                k: v for k, v in tech.items() if k not in ("page", "row", "category_id")
+            }
+            pages[page][category][row].append(item_data)
+
+        def dictify(obj):
+            if isinstance(obj, defaultdict):
+                return {k: dictify(v) for k, v in obj.items()}
+            elif isinstance(obj, dict):
+                return {k: dictify(v) for k, v in obj.items()}
+            return obj
+
+        pages_clean = dictify(pages)
+
+        result = {
+            "pages": {
+                page: {
+                    "categories": {
+                        category: {"rows": rows}
+                        for category, rows in categories.items()
+                    }
+                }
+                for page, categories in pages_clean.items()
+            }
+        }
+
+        # TODO: Clean the elements in a dictionary with unnacessary id and category_id
+        return result
---
+++ src/static/scripts/domContextLoaded.js
@@ -0,0 +1,33 @@
+<!-- FIX: Carousel, tab not working without this component -->
+document.addEventListener("DOMContentLoaded", function () {
+  const carousels = document.querySelectorAll(".carousel");
+  const tabLinks = document.querySelectorAll("[data-mdb-tab-init]");
+  const collapse = document.querySelectorAll("[data-mdb-collapse-init]");
+  const tooltips = document.querySelectorAll("[data-mdb-tooltip-init]");
+
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function () {
+      const btn = document.getElementById("submitBtn");
+      const spinner = document.getElementById("spinnerSubmit");
+
+      btn.disabled = true;
+      spinner.style.display = "inline-block";
+    });
+
+  collapse.forEach((collapse) => {
+    new mdb.Collapse(collapse);
+  });
+
+  tabLinks.forEach((tabLink) => {
+    new mdb.Tab(tabLink);
+  });
+
+  carousels.forEach((carousel) => {
+    new mdb.Carousel(carousel);
+  });
+
+  tooltips.forEach((tooltip) => {
+    new mdb.Tooltip(tooltip);
+  });
+});
