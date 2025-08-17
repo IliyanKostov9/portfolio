ac62c86fd7dd8894736c42c278fc376afe7e8d43
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-08-17 16:59:47 +0200

    fix: table init
    M  src/portfolio/settings.py

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_ROOT = "/var/www/portfolio.ikostov.org/static/"
+STATIC_ROOT = "/var/www/portfolio.ikostov.org/static/"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
