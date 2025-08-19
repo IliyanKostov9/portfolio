73dab6d1e2a5d3e5d09f23f6b9ac2777f8cddc33
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-08-19 22:03:39 +0200

    fix: added tests as a sub-sub module
    M  src/apps/landing_page/tests/__init__.py
    D  src/apps/landing_page/tests/test_contact_me.py
    A  src/apps/landing_page/tests/views/__init__.py
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py

--- src/apps/landing_page/tests/test_contact_me.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class TestContactMeTestCase(TestCase):
-    def test_post(self):
-        print("ok")
-        self.assertEqual(True, True)
--- src/apps/landing_page/tests/views/home/test_contact_me_view.py
+++ src/apps/landing_page/tests/views/home/test_contact_me_view.py
@@ -3,4 +3,5 @@ from django.test import TestCase
 
 class TestContactMeTestCase(TestCase):
     def test_post(self):
+        print("This is kinda cool")
         self.assertEqual(True, True)
