7090be38e8584a24f1bba54add2146a6de0f7fdc
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-08-22 15:57:37 +0000

    Fix/cert dates (#138)
    * fix: workers
    
    M  Dockerfile
    
    * fix: certificates
    
    M  src/apps/landing_page/config/portfolio/certificate.yaml
    M  src/apps/landing_page/data_class/portfolio/certification.py
    M  src/apps/landing_page/migrations/0001_initial.py
    R  src/apps/landing_page/migrations/0002_auto_20250820_2315.py -> src/apps/landing_page/migrations/0002_auto_20250822_1716.py
    M  src/apps/landing_page/models/certification.py
    M  src/apps/landing_page/templates/pages/home/certification.html
    
    * fix: tests
    
    M  src/apps/landing_page/tests/models/portfolio.py

--- src/apps/landing_page/models/certification.py
+++ src/apps/landing_page/models/certification.py
@@ -12,6 +12,7 @@ class Certification(Portfolio):
     date: CharField = CharField("Date of the certificate")
     name: CharField = CharField("Name of the certificate")
     url: CharField = CharField("Url of the certificate to verify")
+    issuer: CharField = CharField("Issuer of the certificate")
     # TODO: Don't manually align this card by using margin
     style: CharField = CharField("Custom css of the certificate", max_length=300)
 
