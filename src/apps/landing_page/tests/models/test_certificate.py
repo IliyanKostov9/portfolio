279355bbd4084dd1364932a69740c8c6b9c7a41f
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-08-23 15:00:07 +0000

    Feat/model tests (#139)
    * feat: added hackerrank icon
    
    M  src/apps/landing_page/templates/pages/base.html
    
    * feat: added more model tests
    
    M  src/apps/landing_page/tests/models/test_certificate.py
    A  src/apps/landing_page/tests/models/test_project.py
    A  src/apps/landing_page/tests/models/test_work_history.py
    
    * feat: changed Makefile
    
    M  Makefile

--- src/apps/landing_page/tests/models/test_certificate.py
+++ src/apps/landing_page/tests/models/test_certificate.py
@@ -51,13 +51,24 @@ class CertificationTestCase(Portfolio):
 
     def test_transform(self):
         self.setUp()
-        # certification_model: Any = self.model.apps.get_model(
-        #     "landing_page", "Certification"
-        # )
+        certification_model: Any = self.model.apps.get_model(
+            "landing_page", "Certification"
+        )
 
         certification = Certification()
-        certification.transform()
+        certificates_transformed = certification.transform()
 
-        # self.assertListEqual(educations, list(educations_db))
+        for certs in certificates_transformed:
+            self.assertTrue(certs.get("row"))
+            self.assertTrue(certs.get("certificate"))
+
+            for cert in certs["certificate"]:
+                cert_model = certification_model.objects.get(name=cert["name"])
+
+                self.assertEqual(cert_model.image, cert["image"])
+                self.assertEqual(cert_model.date, cert["date"])
+                self.assertEqual(cert_model.url, cert["url"])
+                self.assertEqual(cert_model.issuer, cert["issuer"])
+                self.assertEqual(cert_model.style, cert["style"])
 
         super().tearDownClass()
