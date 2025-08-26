3565fa0b035be07a0e450464026c3467ecdbe5b0
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-08-26 17:53:02 +0000

    Refactor/test view (#150)
    * feat: added views
    
    A  src/apps/landing_page/helpers/hash.py
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    A  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * feat: added views
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/views/home/contact_me_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/models/project.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * fix: test issue
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * fix: test issue
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * fix: test issue
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    M  src/portfolio/settings.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    M  src/apps/landing_page/tests/views/home/test_home_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    
    * feat: updated tests
    
    M  src/apps/landing_page/tests/views/home/test_contact_me_view.py
    
    * fix: secret key boolean
    
    M  src/portfolio/settings.py

--- src/apps/landing_page/tests/views/home/test_contact_me_view.py
+++ src/apps/landing_page/tests/views/home/test_contact_me_view.py
@@ -1,6 +1,48 @@
 from django.test import TestCase
+import os
+from django.core.handlers.wsgi import WSGIRequest
+from django.conf import settings
 
 
 class ContactMeTestCase(TestCase):
     def test_post(self):
-        self.assertEqual(True, True)
+        settings.SECRET_KEY = "dummy-key"
+        os.environ["PORTFOLIO_FROM_EMAIL"] = "dummy.dummy@mail.com"
+        os.environ["PORTFOLIO_TO_EMAIL"] = "dummy.dummy@mail.com"
+        os.environ["PORTFOLIO_EMAIL_USER"] = "dummy"
+        os.environ["PORTFOLIO_EMAIL_HOST"] = "smtp.dummy.com"
+        os.environ["PORTFOLIO_EMAIL_PASSWORD"] = "123"
+
+        # expected_headers = """
+        # default-src 'self'; frame-ancestors 'self'; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com; form-action 'self'; img-src 'self'; font-src https://fonts.gstatic.com https://cdnjs.cloudflare.com; style-src 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-3ITP0qhJJYBulKb1omgiT3qOK6k0iB3rMDhGfpM8b7c=' 'sha256-DqHyLrY03A99krj4zwj8j6M04dAkecX+/ck4dgG6zCk=' 'sha256-bsV5JivYxvGywDAZ22EZJKBFip65Ng9xoJVLbBg7bdo=' 'sha256-oxny43U4yMNZqsxffAINTdjzidFj6nAZr/6MrmG+WZA='
+        # """
+
+        response: WSGIRequest = self.client.post(
+            "/home/contact-me/",
+            {
+                "name": "dummy",
+                "email": "dummy.dummy@mail.com",
+                "message": "This is a dummy message by dummy from dummy",
+            },
+            follow=True,
+        )
+
+        # BUG: For some reason the headers is not the same if you run it locally compared to CI
+        # self.assertEqual(
+        #     hash(response.headers.get("Content-Security-Policy-Report-Only").strip()),
+        #     hash(expected_headers),
+        # )
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, "/")
+
+        response: WSGIRequest = self.client.post(
+            "/home/contact-me/",
+            {
+                "name": "dummy",
+                "email": "This is invalid email",
+                "message": "This is a dummy message by dummy from dummy",
+            },
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 400)
