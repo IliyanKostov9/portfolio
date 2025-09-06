65bd38e60c4eb6c8961d8a8a039e17e276c11586
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-09-06 14:26:04 +0000

    Feature/languages (#162)
    * feat: added language components
    
    M  Makefile
    A  src/apps/landing_page/config/portfolio/language.yaml
    A  src/apps/landing_page/data_class/portfolio/language.py
    M  src/apps/landing_page/migrations/0001_initial.py
    R  src/apps/landing_page/migrations/0002_auto_20250822_1716.py -> src/apps/landing_page/migrations/0002_auto_20250906_1512.py
    M  src/apps/landing_page/migrations/__init__.py
    M  src/apps/landing_page/models/__init__.py
    A  src/apps/landing_page/models/language.py
    M  src/apps/landing_page/templates/pages/base.html
    M  src/apps/landing_page/templates/pages/home/index.html
    A  src/apps/landing_page/templates/pages/home/languages.html
    M  src/apps/landing_page/views/home/home_view.py
    M  src/portfolio/monitor/log.py
    
    * feat: added flags
    
    M  src/apps/landing_page/config/portfolio/language.yaml
    M  src/apps/landing_page/data_class/portfolio/language.py
    M  src/apps/landing_page/migrations/0001_initial.py
    R  src/apps/landing_page/migrations/0002_auto_20250906_1512.py -> src/apps/landing_page/migrations/0002_auto_20250906_1547.py
    M  src/apps/landing_page/models/language.py
    M  src/apps/landing_page/templates/pages/home/languages.html
    M  src/portfolio/monitor/log.py
    A  src/templates/icons/bulgaria.svg
    A  src/templates/icons/france.svg
    A  src/templates/icons/germany.svg
    A  src/templates/icons/united-kingdom.svg
    
    * feat: added flags
    
    M  src/apps/landing_page/templates/pages/home/languages.html
    
    * fix: alignment issue with flag image
    
    M  src/apps/landing_page/templates/pages/home/languages.html
    M  src/templates/icons/bulgaria.svg
    M  src/templates/icons/france.svg
    M  src/templates/icons/germany.svg
    M  src/templates/icons/united-kingdom.svg
    
    * feat: finalized lang cards
    
    M  src/apps/landing_page/models/language.py
    M  src/apps/landing_page/static/style/home.scss
    M  src/apps/landing_page/templates/pages/home/languages.html
    
    * fix: test issue
    
    M  src/apps/landing_page/models/language.py
    M  src/apps/landing_page/tests/models/portfolio.py

---
+++ src/apps/landing_page/data_class/portfolio/language.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import Any
+
+from typing_extensions import override
+
+from apps.landing_page.data_class.portfolio.portfolio import Portfolio
+
+
+@dataclass(frozen=True)
+class Language(Portfolio):
+    name: str
+    proficiency: str
+    icon: str
+    row: int
+
+    @classmethod
+    def from_yaml(cls, path: str) -> list["Language"]:
+        objects: Any = super().read_yaml(path)
+
+        return [cls(**obj) for obj in objects]
+
+    @override
+    @staticmethod
+    def table_create(apps):
+        language_model = apps.get_model(Portfolio.app_name, "Language")
+        language_model.objects.all().delete()
+
+        languages: list[Language] = Language.from_yaml("portfolio/language.yaml")
+
+        for language in languages:
+            language_model.objects.create(
+                name=language.name,
+                proficiency=language.proficiency,
+                row=language.row,
+                icon=language.icon,
+            )
