5ebcb87413fc8355d79edcc2a42525242b0eed52
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-09-06 11:51:12 +0000

    Feature/logging (#160)
    * feat: added loguru dep
    
    M  pyproject.toml
    M  uv.lock
    
    * feat: added loki send via loguru
    
    D  docs/.gitkeep
    M  pyproject.toml
    D  scripts/.gitkeep
    M  src/apps/landing_page/views/home/home_view.py
    A  src/portfolio/monitor/log.py
    M  uv.lock
    
    * feat: added logging for the views
    
    M  src/apps/landing_page/views/home/contact_me_view.py
    M  src/apps/landing_page/views/home/home_view.py
    M  src/portfolio/monitor/log.py

--- src/apps/landing_page/views/home/contact_me_view.py
+++ src/apps/landing_page/views/home/contact_me_view.py
@@ -7,15 +7,23 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.views import View
 
+from portfolio.monitor.log import logger
 from apps.landing_page.forms.contact_me_form import ContactMe, ContactMeForm
 
 
 class ContactMeView(View):
+    LOG = logger.bind(module="contact_me_view")
+
     def post(self, request: Any) -> HttpResponse:
+        self.LOG.info(
+            f"User: {request.META.get('REMOTE_ADDR')} is requesting to send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+            code=200,
+        )
+
         form: ContactMeForm = ContactMeForm(request.POST)
 
         if form.is_valid():
-            self._check_email_envs_are_set()
+            self._check_email_envs_are_set(request.META.get("REMOTE_ADDR"))
 
             contact_me: ContactMe = ContactMe.from_form(form)
 
@@ -30,12 +38,22 @@ class ContactMeView(View):
                 "You have successfully sent an email to Iliyan!",
             )
 
+            self.LOG.success(
+                f"User: {request.META.get('REMOTE_ADDR')} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+                code=200,
+            )
+
             return redirect("home")
         else:
             print("Form is invalid for some reason!")
+            self.LOG.error(
+                f"User: {request.META.get('REMOTE_ADDR')} cannot send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}, because of an invalid data he put on the form",
+                code=400,
+            )
+
             return HttpResponseBadRequest()
 
-    def _check_email_envs_are_set(self) -> None:
+    def _check_email_envs_are_set(self, user_ip_address: str) -> None:
         mandatory_env_variables: list[str] = [
             "PORTFOLIO_FROM_EMAIL",
             "PORTFOLIO_TO_EMAIL",
@@ -46,5 +64,9 @@ class ContactMeView(View):
 
         for env_var in mandatory_env_variables:
             if not os.environ.get(env_var):
+                self.LOG.error(
+                    f"Application error: Cannot send an email, because the environment variable {env_var} is missing. Aborting email send request from user {user_ip_address}",
+                    code=500,
+                )
                 # TODO: Redirect user to error page 500
                 raise ValueError(f"Environment variable {env_var} is NOT SET!")
