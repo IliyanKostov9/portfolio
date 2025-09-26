832dd1a43cb55dced0f8ebcf80722a0bfd546ad7
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-09-26 23:20:55 +0200

    Feature/csp report (#169)
    * fix: cert dates
    
    M  src/apps/landing_page/config/portfolio/certificate.yaml
    M  uv.lock
    
    * feat: removed Makefile vars
    
    M  .github/workflows/dependabot_auto_merge.yaml
    M  Makefile
    
    * feat: added flake action
    
    A  .github/workflows/flake-test.yml
    
    * fix: flake action
    
    M  .github/workflows/flake-test.yml
    
    * feat: added sops
    
    A  .env.enc
    A  .sops.yaml
    M  flake.nix
    M  pyproject.toml
    M  src/apps/landing_page/urls.py
    A  src/apps/landing_page/views/monitor/csp_report_view.py
    M  src/portfolio/settings.py
    M  src/portfolio/urls.py
    M  uv.lock
    
    * feat: updated csp report
    
    M  pyproject.toml
    M  src/apps/landing_page/urls.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py
    M  src/portfolio/settings.py
    M  src/portfolio/urls.py
    M  uv.lock
    
    * refactor: email
    
    M  src/apps/landing_page/templates/components/toast.html
    M  src/apps/landing_page/views/home/contact_me_view.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py
    A  src/portfolio/helpers/client.py
    A  src/portfolio/helpers/email.py
    
    * feat: finalized csp report
    
    M  src/apps/landing_page/views/home/contact_me_view.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py
    
    * feat: csp report
    
    M  src/apps/landing_page/urls.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py
    
    * feat: resolve sonar
    
    M  .github/workflows/flake-test.yml

--- src/apps/landing_page/views/home/contact_me_view.py
+++ src/apps/landing_page/views/home/contact_me_view.py
@@ -2,11 +2,12 @@ import os
 from typing import Any
 
 from django.contrib import messages
-from django.core.mail import EmailMessage
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.views import View
 
+from portfolio.helpers.client import get_client_ip
+from portfolio.helpers.email import Email
 from portfolio.monitor.log import logger
 from apps.landing_page.forms.contact_me_form import ContactMe, ContactMeForm
 
@@ -15,58 +16,49 @@ class ContactMeView(View):
     LOG = logger.bind(module="contact_me_view")
 
     def post(self, request: Any) -> HttpResponse:
+        client_ip: str = get_client_ip(request)
+
         self.LOG.info(
-            f"User: {request.META.get('REMOTE_ADDR')} is requesting to send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+            f"User: {client_ip} is requesting to send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
             code=200,
         )
 
         form: ContactMeForm = ContactMeForm(request.POST)
 
         if form.is_valid():
-            self._check_email_envs_are_set(request.META.get("REMOTE_ADDR"))
-
             contact_me: ContactMe = ContactMe.from_form(form)
 
-            _ = EmailMessage(
-                subject=f"{contact_me.email} contacted you from your portfolio 'ContactMe'",
-                body=f"Name: {contact_me.name}\n\n" + contact_me.message,
-                to=[os.environ.get("PORTFOLIO_TO_EMAIL")],
-            ).send()
+            try:
+                Email.send(
+                    subject=f"{contact_me.email} contacted you from your portfolio 'ContactMe'",
+                    message=f"Name: {contact_me.name}\n\n" + contact_me.message,
+                    recipient=os.environ.get("PORTFOLIO_TO_EMAIL"),
+                )
+                messages.success(
+                    request,
+                    "You have successfully sent an email to Iliyan!",
+                )
+                self.LOG.success(
+                    f"User: {client_ip} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+                    code=200,
+                )
 
-            messages.success(
-                request,
-                "You have successfully sent an email to Iliyan!",
-            )
-
-            self.LOG.success(
-                f"User: {request.META.get('REMOTE_ADDR')} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
-                code=200,
-            )
+            except ValueError as error:
+                self.LOG.error(
+                    f"Application error: Cannot send an email,{error} .Aborting email send request from user {client_ip}",
+                    code=500,
+                )
+                messages.error(
+                    request,
+                    "Error in sending your email message. Please try again next time :(",
+                )
 
             return redirect("home")
         else:
             print("Form is invalid for some reason!")
             self.LOG.error(
-                f"User: {request.META.get('REMOTE_ADDR')} cannot send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}, because of an invalid data he put on the form",
+                f"User: {client_ip} cannot send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}, because of an invalid data he put on the form",
                 code=400,
             )
 
             return HttpResponseBadRequest()
-
-    def _check_email_envs_are_set(self, user_ip_address: str) -> None:
-        mandatory_env_variables: list[str] = [
-            "PORTFOLIO_FROM_EMAIL",
-            "PORTFOLIO_TO_EMAIL",
-            "PORTFOLIO_EMAIL_HOST",
-            "PORTFOLIO_EMAIL_USER",
-            "PORTFOLIO_EMAIL_PASSWORD",
-        ]
-
-        for env_var in mandatory_env_variables:
-            if not os.environ.get(env_var):
-                self.LOG.error(
-                    f"Application error: Cannot send an email, because the environment variable {env_var} is missing. Aborting email send request from user {user_ip_address}",
-                    code=500,
-                )
-                # TODO: Redirect user to error page 500
-                raise ValueError(f"Environment variable {env_var} is NOT SET!")
