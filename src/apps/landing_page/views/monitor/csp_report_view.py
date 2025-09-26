b1df985f08b98592374f5e6281896a53c31dfc0e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-09-26 22:31:21 +0200

    refactor: email
    M  src/apps/landing_page/templates/components/toast.html
    M  src/apps/landing_page/views/home/contact_me_view.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py
    A  src/portfolio/helpers/client.py
    A  src/portfolio/helpers/email.py

--- src/apps/landing_page/views/monitor/csp_report_view.py
+++ src/apps/landing_page/views/monitor/csp_report_view.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-import os
+import json
 from typing import Any
 
 from django.views import View
 
+from portfolio.helpers.client import get_client_ip
 from portfolio.monitor.log import logger
 
 
@@ -11,14 +12,17 @@ class CSPReportView(View):
     LOG = logger.bind(module="csp_report_view")
 
     def post(self, request: Any) -> None:
-        self.LOG.info(
-            "Content Security Policy has been violated!",
-            code=200,
-        )
+        client_ip: str = get_client_ip(request)
 
-        self.LOG.success(
-            f"User: {request.META.get('REMOTE_ADDR')} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+        self.LOG.info(
+            f"Content Security Policy has been violated by client: {client_ip}",
             code=200,
         )
+        print(f">>>>>>>>>> {json.loads(request.body)}")
+
+        # self.LOG.success(
+        #     f"User: {request.META.get('REMOTE_ADDR')} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+        #     code=200,
+        # )
 
         return HttpResponse("Report send!")
