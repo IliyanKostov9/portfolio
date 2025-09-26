469c0763fbe927f3585a048960cb1d669414b908
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-09-26 23:12:49 +0200

    feat: csp report
    M  src/apps/landing_page/urls.py
    M  src/apps/landing_page/views/monitor/csp_report_view.py

--- src/apps/landing_page/urls.py
+++ src/apps/landing_page/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 
 from apps.landing_page.views.home.contact_me_view import ContactMeView
 from apps.landing_page.views.home.home_view import HomeView
@@ -8,7 +7,5 @@ from apps.landing_page.views.monitor.csp_report_view import CSPReportView
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("home/contact-me/", ContactMeView.as_view(), name="home/contact-me"),
-    path(
-        "csp-report/", csrf_exempt(CSPReportView.as_view()), name="monitor/csp-report"
-    ),
+    path("csp-report/", CSPReportView.as_view(), name="monitor/csp-report"),
 ]
--- src/apps/landing_page/views/monitor/csp_report_view.py
+++ src/apps/landing_page/views/monitor/csp_report_view.py
@@ -1,23 +1,36 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from typing import Any
 import os
+
+from django.utils.decorators import method_decorator
+
 
 from portfolio.helpers.email import Email
 from django.views import View
 
-from portfolio.helpers.client import get_client_ip
+from portfolio.helpers.client import get_client_ip, get_client_user_agent
 from portfolio.monitor.log import logger
+from django.views.decorators.csrf import csrf_exempt
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class CSPReportView(View):
     LOG = logger.bind(module="csp_report_view")
 
     def post(self, request: Any) -> HttpResponse:
         client_ip: str = get_client_ip(request)
+        client_user_agent: str = get_client_user_agent(request)
         stringified_request = self._stringify_request(request)
 
+        if request.content_type not in ["application/json", "application/csp-report"]:
+            self.LOG.error(
+                f"Ivalid content type made by client: {client_ip} with user agent: {client_user_agent}",
+                code=200,
+            )
+            return HttpResponseBadRequest("Invalid content type")
+
         message: str = f"""
-            Content Security Policy has been violated by client: {client_ip}
+            Content Security Policy has been violated by client: {client_ip} with user agent {client_user_agent}
             Full details: {stringified_request}
             """
 
