e2d202f9a9d7e15c7dabc91c2067654c24199779
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-10-28 22:23:47 +0100

    feat: added translations
    A  src/apps/resume/config/page_content.yaml
    A  src/apps/resume/config/translations.py
    M  src/apps/resume/templates/pages/home/about.html
    M  src/apps/resume/templates/pages/home/index.html
    M  src/apps/resume/views/home/home_view.py
    M  src/locale/bg/LC_MESSAGES/django.po
    A  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po

--- src/apps/resume/views/home/home_view.py
+++ src/apps/resume/views/home/home_view.py
@@ -1,5 +1,7 @@
+import os
 from typing import Any
 
+import yaml
 from django.http import HttpResponse
 from django.template import loader
 from django.utils.translation import get_language
@@ -24,6 +26,8 @@ class HomeView(View):
             code=200,
         )
 
+        content = self._read_yaml("page_content.yaml")
+
         template = loader.get_template("pages/home/index.html")
 
         technologies = Technology().transform()
@@ -33,8 +37,14 @@ class HomeView(View):
         projects = Project().transform()
         languages = Language().transform()
 
+        text = self._translate(content["body"]["about"])
+
+        print("This is test> " + str(text))
+
         context: dict[str, Any] = {
-            "sub_title": _("I try to figure out how things work."),
+            "headers": self._translate(content["headers"]),
+            "tr_main": self._translate(content["body"]["main"]),
+            "tr_about": self._translate(content["body"]["about"]),
             "language": get_language(),
             "technologies": technologies,
             "work_histories": work_histories,
@@ -49,3 +59,23 @@ class HomeView(View):
             code=200,
         )
         return HttpResponse(template.render(context, request))
+
+    def _read_yaml(self, file_name: str) -> Any:
+        if not file_name.endswith((".yaml", ".yml")):
+            raise InterruptedError("File must end with yml or yaml!")
+
+        parent_dir: str = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "config")
+        )
+
+        with open(os.path.join(parent_dir, file_name), "r") as file:
+            return yaml.safe_load(file)
+
+    def _translate(self, obj) -> Any:
+        if isinstance(obj, str):
+            return _(obj)
+        elif isinstance(obj, list):
+            return [self._translate(item) for item in obj]
+        elif isinstance(obj, dict):
+            return {k: self._translate(v) for k, v in obj.items()}
+        return obj
