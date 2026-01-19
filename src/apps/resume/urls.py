3ac74daa192de065c24be2a80e5824610428a411
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-01-19 22:40:46 +0100

    feat: added latex workflow (#215)
    * feat: added latex workflow
    
    A  .github/workflows/latex.yaml
    
    * fix: latex workflow
    
    M  .github/workflows/latex.yaml
    
    fix: latex workflow
    
    M  .github/workflows/latex.yaml
    
    fix: latex workflow
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added upload to s3 action
    
    M  .github/workflows/latex.yaml
    
    feat: added release
    
    M  .github/workflows/docker-publish.yaml
    M  .github/workflows/latex.yaml
    M  Dockerfile
    M  flake.lock
    
    feat: added download bio btn
    
    M  pyproject.toml
    M  src/apps/resume/templates/pages/home/contact_me.html
    A  src/portfolio/models/s3.py
    M  uv.lock
    
    feat: added s3 not working yet
    
    A  main.pdf
    M  src/apps/resume/templates/pages/home/contact_me.html
    M  src/apps/resume/urls.py
    A  src/apps/resume/views/home/cv_download_view.py
    M  src/locale/bg/LC_MESSAGES/django.po
    M  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po
    
    chore: remove main.pdf
    
    D  main.pdf
    
    chore: remove main.pdf
    
    M  .github/workflows/latex.yaml
    M  src/apps/resume/templates/pages/home/contact_me.html
    M  src/apps/resume/views/home/cv_download_view.py
    M  src/portfolio/helpers/email.py
    A  src/portfolio/helpers/utils.py
    M  src/portfolio/models/s3.py
    
    fix: sonar
    
    M  src/apps/resume/templates/pages/home/contact_me.html
    
    chore: remove main.pdf
    
    M  .github/workflows/latex.yaml
    
    fix: langs
    
    M  src/apps/resume/views/home/cv_download_view.py
    M  src/locale/bg/LC_MESSAGES/django.po
    M  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po
    M  src/portfolio/models/s3.py
    
    * fix: langs
    
    M  .github/workflows/docker-publish.yaml
    M  .github/workflows/latex.yaml
    M  Dockerfile

--- src/apps/resume/urls.py
+++ src/apps/resume/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from apps.resume.views.home.contact_me_view import ContactMeView
 from apps.resume.views.home.home_view import HomeView
 from apps.resume.views.monitor.csp_report_view import CSPReportView
+from apps.resume.views.home.cv_download_view import CVDownloadView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("home/contact-me/", ContactMeView.as_view(), name="home/contact-me"),
+    path("home/cv-download/", CVDownloadView.as_view(), name="home/cv-download"),
     path("monitor/csp-report", CSPReportView.as_view(), name="csp-report"),
 ]
