fff2b7bca2b4b91c9d6443c43ab20d6a60ba9b74
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-03-30 22:08:49 +0200

    Feature/add email security for cv (#257)
    * feat: added cv refactor
    
    M  src/apps/resume/templates/pages/home/contact_me.html
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py
    
    * feat: added cv refactor
    
    M  src/apps/resume/templates/pages/home/contact_me.html
    M  src/apps/resume/views/home/contact_me_view.py
    
    feat: added cv refactor
    
    M  src/apps/resume/views/home/contact_me_view.py
    
    feat: added cv refactor
    
    M  src/apps/resume/urls.py
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py
    A  src/portfolio/helpers/security_manager.py
    
    feat: added cv refactor
    
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py
    M  src/portfolio/helpers/email.py
    
    feat: adjusted lang in cv
    
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py
    
    feat: adjusted lang in cv
    
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py
    M  src/locale/bg/LC_MESSAGES/django.po
    M  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po
    
    feat: adjusted lang in cv
    
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/portfolio/helpers/email.py
    
    feat: adjusted lang in cv
    
    M  src/apps/resume/tests/views/home/test_contact_me_view.py
    M  src/apps/resume/views/home/contact_me_view.py
    
    * feat: adjusted lang in cv
    
    M  src/apps/resume/views/home/contact_me_view.py
    M  src/apps/resume/views/home/cv_download_view.py

--- src/apps/resume/views/home/contact_me_view.py
+++ src/apps/resume/views/home/contact_me_view.py
@@ -7,9 +7,10 @@ from django.shortcuts import redirect
 from django.utils.translation import gettext as _
 from django.views import View
 
+from django.utils.translation import get_language
 from apps.resume.forms.contact_me_form import ContactMe, ContactMeForm
-from portfolio.helpers.client import get_client_ip
 from portfolio.helpers.email import Email
+from portfolio.helpers.security_manager import SecurityManager
 from portfolio.monitor.log import logger
 
 
@@ -17,50 +18,102 @@ class ContactMeView(View):
     LOG = logger.bind(module="contact_me_view")
 
     def post(self, request: Any) -> HttpResponse:
-        client_ip: str = get_client_ip(request)
+        form: ContactMeForm = ContactMeForm(request.POST)
 
         self.LOG.info(
-            f"User: {client_ip} is requesting to send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+            f"User is requesting to send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
             code=200,
         )
-
-        form: ContactMeForm = ContactMeForm(request.POST)
 
         if form.is_valid():
             contact_me: ContactMe = ContactMe.from_form(form)
 
-            try:
-                Email.send(
-                    subject=f"{contact_me.email} contacted you from your portfolio 'ContactMe'",
-                    message=f"Name: {contact_me.name}\n\n" + contact_me.message,
-                    recipient=os.environ.get("PORTFOLIO_TO_EMAIL"),
-                )
-                messages.success(
+            if request.POST.get("submitContactmeBtn"):
+                return self.__send_email_to_iliyan(
                     request,
+                    f"{contact_me.email} contacted you from your portfolio 'ContactMe'",
+                    f"Name: {contact_me.name}</br>Message: {contact_me.message}",
                     _("You have successfully sent an email to Iliyan!"),
                 )
-                self.LOG.success(
-                    f"User: {client_ip} has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
-                    code=200,
+            elif request.POST.get("submitCVDownloadBtn"):
+                token: str = SecurityManager.generate_token(
+                    os.environ.get("PORTFOLIO_TO_EMAIL")
                 )
 
-            except ValueError as error:
+                http_protocol: str = (
+                    "https" if os.environ.get("SECURE_SSL_REDIRECT") else "http"
+                )
+
+                host: str = os.environ.get("PORTFOLIO_HOST")
+                if host == "localhost":
+                    host += ":8000"
+
+                url_yes: str = (
+                    f"{http_protocol}://{host}/home/cv-download/{token}/?choice=yes&email={contact_me.email}&language={get_language()}"
+                )
+                url_no: str = (
+                    f"{http_protocol}://{host}/home/cv-download/{token}/?choice=no&email={contact_me.email}&language={get_language()}"
+                )
+
+                Email.send(
+                    subject=_("Thanks for contacting Iliyan!"),
+                    message=_(
+                        "We've contacted Iliyan with your request to download his CV. Please make sure to watch your email box for the status of this request\n\nThanks and have a nice day!"
+                    ),
+                    recipient=contact_me.email,
+                )
+
+                return self.__send_email_to_iliyan(
+                    request,
+                    f"{contact_me.email} has made a request to download your CV",
+                    f"Name: {contact_me.name}</br>Message: {contact_me.message}</br></br>Accept/Refuse: (<a href='{url_yes}'>Yes</a>/<a href='{url_no}'>No</a>)",
+                    _(
+                        "You have successfully sent a CV download request to Iliyan!\n Please wait for him to either accept/refuse your request.\n\nPlease make sure to look at your junk folder, that's where the email may end up in.\nWe'll make sure to update you regarding the status in your email box :)"
+                    ),
+                )
+            else:
                 self.LOG.error(
-                    f"Application error: Cannot send an email,{error} .Aborting email send request from user {client_ip}",
-                    code=500,
+                    "Unrecognized POST request: Incorrect button id!",
+                    code=400,
                 )
                 messages.error(
                     request,
-                    _(
-                        "Error in sending your email message. Please try again next time :("
-                    ),
+                    _("Application error! Sorry for the inconvenience!"),
                 )
 
-            return redirect("home")
         else:
             self.LOG.error(
-                f"User: {client_ip} cannot send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}, because of an invalid data he put on the form",
+                f"User cannot send an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}, because of invalid data he put on the form",
                 code=400,
             )
 
             return HttpResponseBadRequest()
+
+    def __send_email_to_iliyan(
+        self, request: Any, subject: str, message: str, success_message: str
+    ) -> HttpResponse:
+        try:
+            Email.send(
+                subject=subject,
+                message=message,
+                recipient=os.environ.get("PORTFOLIO_TO_EMAIL"),
+                send_as_html=True,
+            )
+
+            self.LOG.success(
+                f"User has successfully sent an email to {os.environ.get('PORTFOLIO_TO_EMAIL')}",
+                code=200,
+            )
+            messages.success(request, success_message)
+
+        except ValueError as error:
+            self.LOG.error(
+                f"Application error: Cannot send an email,{error}. Aborting email send...",
+                code=500,
+            )
+            messages.error(
+                request,
+                _("Error in sending your email message. Please try again next time :("),
+            )
+
+        return redirect("home")
