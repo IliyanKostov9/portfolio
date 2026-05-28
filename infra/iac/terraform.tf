e5089145ba65c35f2e281cad7edb2b441ee994ec
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-05-28 16:20:23 +0200

    feat: changed terraform cloud to s3 state sync
    M  infra/iac/terraform.tf

--- infra/iac/terraform.tf
+++ infra/iac/terraform.tf
@@ -5,19 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.41"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.2"
-    }
-
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.3"
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.11"
     }
   }
 
   backend "s3" {
-    bucket = "tf-state-405466951648"
+    bucket = "tf-state-portfolio-405466951648"
     key    = "states/prod/terraform.tfstate"
     region = "eu-west-1"
 
