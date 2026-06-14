48197064c1d289eb9650fe7170478def2a026e1e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-06-14 14:43:38 +0200

    fix: voice issue
    M  src/apps/blogs/urls.py
    A  src/apps/blogs/views/home/voice_view.py
    M  src/apps/resume/static/style/home.scss
    M  src/apps/resume/views/home/voice_view.py
    A  src/portfolio/helpers/voice.py
    M  src/static/style/default.scss

--- src/apps/blogs/urls.py
+++ src/apps/blogs/urls.py
@@ -5,6 +5,7 @@ from apps.blogs.views.blogs.living_abroad import (
     LivingAbroadView,
 )
 from apps.blogs.views.monitor.csp_report_view import CSPReportView
+from apps.blogs.views.home.voice_view import VoiceView
 
 urlpatterns = [
     path("", BlogsView.as_view(), name="blogs"),
@@ -12,6 +13,11 @@ urlpatterns = [
         "living-abroad",
         LivingAbroadView.as_view(),
         name="living_abroad",
+    ),
+    path(
+        "home/voice/",
+        VoiceView.as_view(),
+        name="home/voice",
     ),
     path("monitor/csp-report", CSPReportView.as_view(), name="csp-report"),
 ]
--- src/apps/blogs/views/home/voice_view.py
+++ src/apps/blogs/views/home/voice_view.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from typing import Any
 from django.views import View
 
-from portfolio.helpers.voice import generate_text_to_speech
 from portfolio.monitor.log import logger
+
+from portfolio.helpers.voice import generate_text_to_speech
 
 
 class VoiceView(View):
