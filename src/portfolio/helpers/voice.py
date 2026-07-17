833523213ae33ebdbbbde715a2dfc134ece7aeb5
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-07-17 21:19:43 +0300

    fix: resolve sonar warnings
    M  src/apps/blogs/templates/pages/blogs/living_abroad.html
    M  src/portfolio/helpers/voice.py
    M  src/static/style/default.scss

--- src/portfolio/helpers/voice.py
+++ src/portfolio/helpers/voice.py
@@ -8,14 +8,14 @@ from django.utils.translation import gettext as _
 from portfolio.models.aws.polly import Polly
 
 
-def generate_text_to_speech(request: Any, LOG: Any) -> bytes | None:
+def generate_text_to_speech(request: Any, log: Any) -> bytes | None:
     try:
         response: Any = json.loads(request.body)
         polly = Polly(os.environ.get("PORTFOLIO_S3_TEXT_TO_SPEECH_PROD_BUCKET"))
 
         text: str = response.get("text").strip()
 
-        LOG.info(
+        log.info(
             f"Received a request to perform text-to-voice for text: {text}",
         )
         stream: bytes = polly.generate(text)
@@ -24,7 +24,7 @@ def generate_text_to_speech(request: Any, LOG: Any) -> bytes | None:
         return stream
 
     except (json.JSONDecodeError, AttributeError) as err:
-        LOG.error(
+        log.error(
             f"No text is found on the post request: {request}: {err}!",
         )
         messages.error(
