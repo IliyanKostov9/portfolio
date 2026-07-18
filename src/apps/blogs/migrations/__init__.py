f062f4118b351ae4aa6ae7e0d5bd062695c99a23
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-07-18 17:00:18 +0300

    feat: migrated changes to this branch (#357)
    * feat: migrated changes to this branch
    
    M  .dockerignore
    M  docs/langs/bg/experience.tex
    M  docs/langs/en/experience.tex
    M  docs/langs/fr/experience.tex
    M  docs/langs/ge/experience.tex
    M  docs/main.tex
    A  docs/profile.jpeg
    D  docs/profile.jpg
    A  fly.toml
    M  src/apps/blogs/config/blog.yaml
    M  src/apps/blogs/migrations/0001_initial.py
    M  src/apps/blogs/migrations/__init__.py
    M  src/apps/blogs/static/style/blogs.scss
    M  src/apps/blogs/templates/pages/blogs/index.html
    A  src/apps/blogs/templates/pages/blogs/living_abroad.html
    D  src/apps/blogs/templates/pages/blogs/the_start_of_something_new.html
    M  src/apps/blogs/tests/views/blogs/test_blogs_view.py
    M  src/apps/blogs/urls.py
    M  src/apps/blogs/views/blogs/blogs_view.py
    A  src/apps/blogs/views/blogs/living_abroad.py
    D  src/apps/blogs/views/blogs/the_start_of_something_new_view.py
    A  src/apps/blogs/views/home/voice_view.py
    M  src/apps/resume/migrations/0001_initial.py
    M  src/apps/resume/migrations/0002_resume_migrate.py
    M  src/apps/resume/static/style/home.scss
    M  src/apps/resume/templates/pages/home/index.html
    M  src/apps/resume/views/home/voice_view.py
    M  src/locale/bg/LC_MESSAGES/django.po
    M  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po
    A  src/portfolio/helpers/voice.py
    M  src/portfolio/settings.py
    A  src/static/images/blogs/driving-with-movers.jpg
    A  src/static/images/blogs/me_in_march_2025.jpg
    A  src/static/images/blogs/new_apartment_1.jpg
    A  src/static/images/blogs/new_apartment_2.jpg
    A  src/static/images/blogs/new_apartment_3.jpg
    A  src/static/images/blogs/new_apartment_4.jpg
    A  src/static/images/blogs/new_apartment_5.jpg
    A  src/static/images/blogs/new_apartment_6.jpg
    A  src/static/images/blogs/nice-ville.jpg
    A  src/static/images/blogs/plane_in_sunlight.jpg
    A  src/static/images/blogs/varna-beach.jpg
    D  src/static/images/blogs/zprofile.png
    A  src/static/images/profile.jpeg
    D  src/static/images/profile.jpg
    M  src/static/scripts/domContextLoaded.js
    M  src/static/style/default.scss
    M  src/templates/base.html
    M  uv.lock
    
    * feat: translation
    
    M  src/locale/bg/LC_MESSAGES/django.po
    M  src/locale/en/LC_MESSAGES/django.po
    M  src/locale/fr/LC_MESSAGES/django.po
    M  src/locale/ge/LC_MESSAGES/django.po

--- src/apps/blogs/migrations/__init__.py
+++ src/apps/blogs/migrations/__init__.py
@@ -2,12 +2,12 @@ from apps.blogs.data_class.blog import Blog
 from apps.blogs.data_class.blog_category import BlogCategory
 
 
-def init(apps, _):
+def init(apps, schema_editor):
     BlogCategory.table_create(apps)
     Blog.table_create(apps)
 
 
-def init_reverse():
+def init_reverse(apps, schema_editor):
     """
     NOTE: Keep it because it'll complain when you make `make sql-init-test`
     """
