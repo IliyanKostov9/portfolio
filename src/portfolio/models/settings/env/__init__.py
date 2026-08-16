45e1d1374409ef8011c637a741a9ed4369cabf02
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-08-16 15:54:47 +0300

    Chore/arrange configuration (#393)
    * feat: added tf modules
    
    M  infra/iac/main.tf
    A  infra/iac/modules/aws/host_web_assets/main.tf
    A  infra/iac/modules/aws/host_web_assets/variables.tf
    M  uv.lock
    
    * feat: added django storages
    
    M  pyproject.toml
    M  src/portfolio/settings.py
    M  uv.lock
    
    * feat: added django configurations
    
    M  pyproject.toml
    M  src/apps/blogs/migrations/0001_initial.py
    M  src/apps/blogs/migrations/0002_blogs_migrate.py
    M  src/apps/blogs/tests/views/blogs/test_blogs_view.py
    M  src/apps/blogs/tests/views/monitor/test_csp_report_view.py
    M  src/apps/resume/migrations/0001_initial.py
    M  src/apps/resume/migrations/0002_resume_migrate.py
    M  src/apps/resume/tests/views/home/test_contact_me_view.py
    M  src/apps/resume/tests/views/home/test_home_view.py
    M  src/apps/resume/tests/views/home/test_voice_view.py
    M  src/apps/resume/tests/views/monitor/test_csp_report_view.py
    M  src/manage.py
    M  src/portfolio/asgi.py
    A  src/portfolio/configure.py
    A  src/portfolio/models/settings/env/__init__.py
    A  src/portfolio/models/settings/env/common.py
    A  src/portfolio/models/settings/env/dev.py
    A  src/portfolio/models/settings/env/prod.py
    D  src/portfolio/settings.py
    D  src/portfolio/wsgi.py
    M  uv.lock
    
    * feat: remove cfn
    
    D  infra/iac/modules/aws/host_web_assets/main.tf
    D  infra/iac/modules/aws/host_web_assets/variables.tf
    
    * feat: remove cfn
    
    M  infra/iac/main.tf

---
+++ src/portfolio/models/settings/env/__init__.py
@@ -0,0 +1,2 @@
+from .dev import Dev  # noqa
+from .prod import Prod  # noqa
---
+++ src/portfolio/models/settings/env/prod.py
@@ -0,0 +1,48 @@
+import os
+from typing import Final
+
+from configurations import Configuration
+
+from .common import Common
+
+ADMIN: Final[str] = "Iliyan"
+
+
+class Prod(Common, Configuration):
+    DEBUG = False
+    CONTENT_SECURITY_POLICY = Common.CSP_POLICY
+    ADMINS = [(ADMIN, os.environ.get("PORTFOLIO_TO_EMAIL"))]
+    MANAGERS = [(ADMIN, os.environ.get("PORTFOLIO_TO_EMAIL"))]
+    SERVER_EMAIL = os.environ.get("PORTFOLIO_FROM_EMAIL")
+
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 3600  # TODO: Increase it ?
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    CONN_MAX_AGE = None
+    COMPRESS_ENABLED = True
+    COMPRESS_OFFLINE = True
+    STATIC_ROOT = "/var/www/portfolio.ikostov.org/static/"
+    COMPRESS_ROOT = STATIC_ROOT
+    COMPRESS_OUTPUT_DIR = "CACHE"
+
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # NOTE: Maybe remove it from prod ?
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "portfolio-cache",
+        }
+    }
+    STORAGES = {
+        "default": {"BACKEND": "storages.backends.s3.S3Storage"},
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+    INSTALLED_APPS = Common.apps[: len(Common.apps) - 3]
+
+    print("Running in production. Now setting all prod options ON...")
