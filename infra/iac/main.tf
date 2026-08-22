254e1abaabc31cad357201b71d87c88ddb7e9367
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-08-22 12:51:31 +0300

    feat: tf action
    M  infra/iac/main.tf

--- infra/iac/main.tf
+++ infra/iac/main.tf
@@ -4,17 +4,6 @@ module "s3_main_storage" {
   name   = "main"
 }
 
-import {
-  to = module.s3_main_storage.aws_s3_bucket.current
-  id = format("%s-%s-%s", "main", var.env, var.account_id)
-}
-
-import {
-  to = module.s3_main_storage.aws_iam_user.current
-  id = format("portfolio-user-%s-%s", "main", var.env)
-}
-
-
 module "s3_polly" {
   source = "./modules/aws/file_storage"
   env    = var.env
@@ -35,10 +24,5 @@ module "ses_email_identity_one" {
   domain             = var.domain
   zone_id            = var.zone_id
   ses_identity_email = var.ses_identity_email
-}
-
-import {
-  to = module.ses_email_identity_one.aws_iam_user.current
-  id = format("ses-user-%s", var.env)
 }
 
