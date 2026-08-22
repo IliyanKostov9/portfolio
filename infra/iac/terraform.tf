91067015140be265241795cbde2c7549cb143a3c
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-08-22 10:28:10 +0300

    feat: added secrets
    M  infra/iac/terraform.tf

--- infra/iac/terraform.tf
+++ infra/iac/terraform.tf
@@ -12,8 +12,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "tf-state-portfolio-944850789927"
-    key    = "states/prod/terraform.tfstate"
+    bucket = "tf-state-aws-944850789927"
+    key    = "states/portfolio/prod/terraform.tfstate"
     region = "eu-west-1"
 
     dynamodb_table = "terraform-state-locks"
